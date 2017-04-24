d4dc2c17a64e20d393da2f85a0afe961d7bc804e
Joseph Crandal <crandalj@gmail.com>
2017-04-24 00:11:39 -0400

    V1.1 Level 2 bug
    Lava wasn't registering collision because groundcheck stops player from hitting it. Made script with Trigger logic for Lava to avoid other areas breaking by changing the TouchOfDeath script.

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other){
+		//Debug.Log (other.gameObject.name);
+		if (other.gameObject.tag == "Player") {
+			//Trigger level complete.
+			PlayerController.player.canMove = false;
+			GameController.gamecontroller.RetryMenu();
+		}
+	}
+}
